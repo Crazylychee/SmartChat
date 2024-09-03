e8b4c524ba4171d885573691b355bcc6a5ccb95a
catnaut <catnaut@outlook.com>
2024-09-03 14:30:00 +0800

    fix(front-end): 修复类型增强失效

--- front-end/smartchat/src/renderer/src/env.d.ts
+++ front-end/smartchat/src/renderer/src/env.d.ts
@@ -1,5 +1,5 @@
-import EmojiPicker from 'vue3-emoji-picker'
-import 'vue3-emoji-picker/css'
+// import EmojiPicker from 'vue3-emoji-picker'
+// import 'vue3-emoji-picker/css'
 /// <reference types="vite/client" />
 
 declare module '*.vue' {
@@ -7,4 +7,13 @@ declare module '*.vue' {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>
   export default component
+}
+
+interface ImportMetaEnv {
+  // readonly VITE_TEST: string
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
 }
