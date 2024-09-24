b546dd9056cb11ba04653503bb7fa51c6bbd84c0
catnaut <catnaut@outlook.com>
2024-09-25 02:35:25 +0800

    fix: 修复 ListChat 响应式Bug

--- front-end/smartchat/src/renderer/src/components/layout/ListWrapper/ListChat.vue
+++ front-end/smartchat/src/renderer/src/components/layout/ListWrapper/ListChat.vue
@@ -2,7 +2,7 @@
   <perfect-scrollbar>
     <div class="chat-list">
       <ListChatItem
-        v-for="(value, key) in chatLists"
+        v-for="(value, key) in useChatStore.latestMessages"
         :key="key"
         :user-id="key"
         :context="value.text"
@@ -10,19 +10,17 @@
       />
     </div>
     <WeNoData
-      v-if="!Object.keys(chatLists).length"
+      v-if="!Object.keys(useChatStore.latestMessages).length"
       text="无结果"
       :image-style="{ color: '#ccc' }"
     />
   </perfect-scrollbar>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import useStore from '@/store'
 const { useChatStore } = useStore()
 import ListChatItem from '@/components/common/ListChatItem.vue'
-
-const chatLists = useChatStore.latestMessages
 </script>
 
 <style lang="less" scoped>
