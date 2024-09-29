cbfe4752f48443e7aecde0e1972841edf0bfe86f
catnaut <catnaut@outlook.com>
2024-09-29 18:39:32 +0800

    chore: 增加 vite plugin 实现自动注入 vue devtool Stand alone script

--- front-end/smartchat/electron.vite.config.ts
+++ front-end/smartchat/electron.vite.config.ts
@@ -13,10 +13,28 @@ export default defineConfig({
     resolve: {
       alias: {
         // '@/*': resolve(__dirname,'src/renderer/src/*')
-        '@': resolve(__dirname, './src/renderer/src'),
+        '@': resolve(__dirname, './src/renderer/src')
         // 'turms-client-js': 'file:D:/client-js/turms/turms-client-js'
       }
     },
-    plugins: [vue()]
+    plugins: [
+      vue(),
+      {
+        name: 'inject-dev-script',
+        transformIndexHtml(html) {
+          // 在开发模式下注入 Vue Devtools standalone script
+          if (process.env.NODE_ENV === 'development') {
+            return html.replace(
+              '</head>',
+              `
+              <script src="http://localhost:8098" async></script>
+              </head>
+              `
+            )
+          }
+          return html
+        }
+      }
+    ]
   }
 })
